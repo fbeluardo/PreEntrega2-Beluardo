c79a0c784ff9bb66279a25698d6d47975a1207b5
fedebeluardo <federico.beluardo@gmail.com>
2023-02-25 14:03:56 -0300

    version5

--- js/index.js
+++ js/index.js
@@ -1,12 +1,32 @@
-const VALOR_IVA = 0.21;
+//PRECIO FINAL CON IVA Y CON DESCUENTO EN PESOS ARGENTINOS//
+// const VALOR_IVA = 0.21;
 
-const PRODUCTO = prompt("Ingresa el nombre del producto");
-const PRECIO = prompt("Ingresa el precio del producto");
-const DESCUENTO_PESOS = prompt ("Ingrese el descuento del producto en pesos argentinos")
-const TOTAL = (PRECIO - DESCUENTO_PESOS) * (1 + VALOR_IVA);
+// const PRODUCTO = prompt("Ingresa el nombre del producto");
+// const PRECIO = prompt("Ingresa el precio del producto");
+// const DESCUENTO_PESOS = prompt ("Ingrese el descuento del producto en pesos argentinos")
+// const TOTAL = (PRECIO - DESCUENTO_PESOS) * (1 + VALOR_IVA);
 
-const PRECIO_FINAL =
-  "El precio final de los " + PRODUCTO + " es de: $" + TOTAL + " pesos argentinos";
+// const PRECIO_FINAL =
+//   "El precio final de los " + PRODUCTO + " es de: $" + TOTAL + " pesos argentinos";
 
-alert(PRECIO_FINAL);
+// alert(PRECIO_FINAL);
+
+// const PRECIO = prompt("Ingresa el precio del producto");
+// const PORCENTAJE = prompt ("Ingrese el descuento en porcentaje")
+// function DESCUENTO (PRECIO,PORCENTAJE) {
+//     console.log (DESCUENTO)
+// }
+
+
+const USUARIO_UNO = parseFloat(prompt("Ingresar año de nacimiento del primer usuario registrado"));
+const USUARIO_DOS = parseFloat(prompt("Ingresar año de nacimiento del segundo usuario registrado"));
+const USUARIO_TRES = parseFloat(prompt("Ingresar año de nacimiento del tercer usuario registrado"));
+const USUARIO_CUATRO = parseFloat(prompt("Ingresar año de nacimiento del cuarto usuario registrado"));
+const ANIO_ACTUAL = 2023
+
+const EDAD = ANIO_ACTUAL 
+
+
+
+
 
