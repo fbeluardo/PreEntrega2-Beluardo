087ca6d855f46b2ee5d290d822125c6b6a9c2d1b
fedebeluardo <federico.beluardo@gmail.com>
2023-02-28 10:00:52 -0300

    version5.6

--- js/index.js
+++ js/index.js
@@ -4,19 +4,19 @@
 let suma = 0;
 
 for (let i = 0; i < 4; i++) { 
-    const anioDeNacimiento = parseInt(prompt("Ingresar año de nacimiento del usuario " + (i+1)));
+    const anioDeNacimiento = parseInt(prompt("Por favor ingrese año de nacimiento del usuario " + (i+1)));
     const edad = 2023 - anioDeNacimiento;
     suma += edad; 
     if (edad <18) {
-        alert ("USTED NO PUEDE ACCEDER AL SITIO WEB");}
+        alert ("Usted no puede acceder al Sitio Web");}
     else{
-        alert ("USTED PUEDE ACCEDER AL SITIO WEB");}
+        alert ("Usted puede acceder al Sitio Web");}
 }
 const promedio = parseInt(suma / 4);
 alert("El promedio de las 4 edades es de " + promedio)
 
 function InicioSesionPais () {
-const PAIS = prompt("Ingresa tu país de origen?");
+const PAIS = prompt("Ingrese su país de origen");
 if (PAIS == "Argentina"){
 alert("Bienvenido!");} 
 else {
