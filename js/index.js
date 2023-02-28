8161d76be2834b572149c240d61f2c7cd5b88707
fedebeluardo <federico.beluardo@gmail.com>
2023-02-27 21:41:15 -0300

    version6.2

--- js/index.js
+++ js/index.js
@@ -1,21 +1,3 @@
-// EJEMPLO N° 1 - PRECIO FINAL CON IVA Y CON DESCUENTO EN PESOS ARGENTINOS//
-// const VALOR_IVA = 0.21;
-
-// const PRODUCTO = prompt("Ingresa el nombre del producto");
-// const PRECIO = prompt("Ingresa el precio del producto");
-// const DESCUENTO_PESOS = prompt ("Ingrese el descuento del producto en pesos argentinos")
-// const TOTAL = (PRECIO - DESCUENTO_PESOS) * (1 + VALOR_IVA);
-
-// const PRECIO_FINAL =
-//   "El precio final de los " + PRODUCTO + " es de: $" + TOTAL + " pesos argentinos";
-
-// alert(PRECIO_FINAL);
-
-// const PRECIO = prompt("Ingresa el precio del producto");
-// const PORCENTAJE = prompt ("Ingrese el descuento en porcentaje")
-// function DESCUENTO (PRECIO,PORCENTAJE) {
-//     console.log (DESCUENTO)
-// }
 
 //EJEMPLO N° 2 - EDADES Y PROMEDIO DE EDAD DE USUARIOS - SITIO WEB DE CERVECERIA QUILMES//
 
