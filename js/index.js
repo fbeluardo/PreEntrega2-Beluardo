15fc808130ed19d62d8b2738860e9f7c799aca92
fedebeluardo <federico.beluardo@gmail.com>
2023-03-12 13:20:15 -0300

    version6.5

--- js/index.js
+++ js/index.js
@@ -1,51 +1,55 @@
+// EMPLEADOS DE LA CERVECERIA - TOTAL DE EMPLEADOS DE LA EMPRESA
 
-//EJEMPLO N° 1 - EDADES Y PROMEDIO DE EDAD DE USUARIOS - SITIO WEB CERVECERIA//
-
-let suma = 0;
-
-for (let i = 0; i < 4; i++) { 
-    const anioDeNacimiento = parseInt(prompt("Por favor ingrese año de nacimiento del usuario " + (i+1)));
-    const edad = 2023 - anioDeNacimiento;
-    suma += edad; 
-    if (edad <18) {
-        alert ("Usted no puede acceder al Sitio Web");}
-    else{
-        alert ("Usted puede acceder al Sitio Web");}
-}
-const promedio = parseFloat(suma / 4);
-alert("El promedio de las 4 edades es de " + promedio)
-
-function paisOrigen () {
-const PAIS = prompt("Ingrese su país de origen").toUpperCase()
-if (PAIS == "ARGENTINA"){
-alert("Bienvenido!");} 
-else {
-alert("No se le permite el ingreso");
-}
-}
-paisOrigen();
-
-function saludar (){
-    document.write ("Muchas gracias por la visita!")
-}
-
-saludar ()
-
-
-
-
-
+class Empleado {
+   constructor(nombreEmpleado, edadEmpleado, cargoEmpleado, salarioNeto, antiguedadAnios) {
+     this.nombreEmpleado = nombreEmpleado;
+     this.edadEmpleado = edadEmpleado;
+     this.cargoEmpleado = cargoEmpleado;
+     this.salarioNeto = salarioNeto;
+     this.antiguedadAnios = antiguedadAnios;
+   }
+  }
+  function ingresoEmpleados () {
+    let numeroEmpleados = parseInt (
+      prompt ("Cuantos empleados tiene la empresa?")
+    )
+    let empleados = [];
+    for (let index = 0; index < numeroEmpleados; index++) {
+    let nombreEmpleado = prompt("Ingrese su nombre completo");
+    let edadEmpleado = prompt("Ingrese su edad");
+    let cargoEmpleado = prompt("Ingrese su cargo en la Cerveceria Bariloche");
+    let salarioNeto = prompt("Ingrese su salario neto en la Cerveceria Bariloche");
+    let antiguedadAnios = prompt("Ingrese su antigüedad en la Cerveceria Bariloche");
+    let ingresoEmpleados = new Empleado(
+    nombreEmpleado,
+    edadEmpleado,
+    cargoEmpleado,
+    salarioNeto,
+    antiguedadAnios);
+    empleados.push(ingresoEmpleados);}
+    }
+    ingresoEmpleados()
+    
+    function promedioSalarioEmpleados (empleados) 
+    let 
+ 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+ 
+//  function calcularCosto(productos) {
+//    let sumatoriaCosto = 0;
+//    for (const producto of productos) {
+//      sumatoriaCosto += producto.calcularCosto();
+//    }
+//    return sumatoriaCosto;
+//  }
+ 
+//  function main() {
+//    let productos = agregarProductos();
+//    mostrarProductos(productos);
+//    let costoAlmacen = calcularCosto(productos);
+//    alert("El costo total del almacén es: " + costoAlmacen);
+//  }
+ 
+//  main();
+ 
