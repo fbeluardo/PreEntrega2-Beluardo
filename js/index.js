8dd628843134e94ba6da8eafab9ab0bef66c14bf
fedebeluardo <federico.beluardo@gmail.com>
2023-03-31 19:58:33 -0300

    version10.2

--- js/index.js
+++ js/index.js
@@ -1,35 +1,80 @@
-// Get the product list and cart elements
-const productList = document.querySelector('.product-list');
-const cart = document.querySelector('.cart');
-
-// Initialize the cart object and total price
-let cartItems = [];
-let cartTotal = 0;
-
-// Check if a cart already exists in local storage and load it if it does
-if (localStorage.getItem('cartItems')) {
-  cartItems = JSON.parse(localStorage.getItem('cartItems'));
-  cartTotal = JSON.parse(localStorage.getItem('cartTotal'));
-  updateCart();
-}
-
-function addItem(item) {
-  // Check if the item already exists in the cart
-  let existingItem = cartItems.find(i => i.id === item.id);
-  if (existingItem) {
-    // If the item already exists, increment its quantity
-    existingItem.quantity++;
-  } else {
-    // If the item does not exist, add it to the cart
-    cartItems.push({
-      id: item.id,
-      name: item.name,
-      price: item.price,
-      quantity: 1
-    });
+// Define the products
+const products = [
+  {
+    id: 1,
+    name: "Product 1",
+    price: 10
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    price: 15
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    price: 20
   }
-  
-  // Update the cart and save to local storage
-  updateCart();
-  saveCart();
+];
+
+// Initialize the cart and total
+let cart = [];
+let total = 0;
+
+// Select the HTML elements
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const clearCartButton = document.querySelector(".clear-cart");
+
+// Function to display the cart items
+function displayCart() {
+  // Clear the cart items list
+  cartItems.innerHTML = "";
+
+  // Loop through the cart array and create a new li element for each item
+  cart.forEach(item => {
+    const li = document.createElement("li");
+    li.innerText = `${item.name} - $${item.price}`;
+    cartItems.appendChild(li);
+  });
+
+  // Update the total
+  cartTotal.innerText = `$${total.toFixed(2)}`;
 }
+
+// Function to add an item to the cart
+function addItemToCart(id) {
+  // Find the product with the matching id
+  const product = products.find(product => product.id === id);
+
+  // Add the product to the cart
+  cart.push(product);
+
+  // Update the total
+  total += product.price;
+
+  // Display the cart items
+  displayCart();
+}
+
+// Function to clear the cart
+function clearCart() {
+  // Remove all items from the cart and reset the total
+  cart = [];
+  total = 0;
+
+  // Display the cart items
+  displayCart();
+}
+
+// Add event listeners to the add to cart buttons
+const addToCartButtons = document.querySelectorAll(".add-to-cart");
+addToCartButtons.forEach(button => {
+  button.addEventListener("click", event => {
+    const id = parseInt(event.target.dataset.id);
+    addItemToCart(id);
+  });
+});
+
+// Add event listener to the clear cart button
+clearCartButton.addEventListener("click", clearCart);
