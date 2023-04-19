902fe891f6e545d96d0dabafef388cf0ae6fa127
fedebeluardo <federico.beluardo@gmail.com>
2023-04-19 09:32:26 -0300

    version16.0

--- js/index.js
+++ js/index.js
@@ -92,41 +92,64 @@ window.onload = function() {
     mostrarCarrito();
   }
 }
-
-// FUNCIÓN PARA MOSTRAR EL CARRITO ******************************************************************************
+// FUNCIÓN MOSTRAR CARRITO *************************************************************************************
 function mostrarCarrito() {
 
-// LIMPIAR EL CARRITO DE PRODUCTOS ******************************************************************************
-productosCarrito.innerHTML = "";
+  // LIMPIAR EL CARRITO DE PRODUCTOS****************************************************************************
+  productosCarrito.innerHTML = "";
 
-// CREAR UN OBJETO VACÍO PARA CONTENER EL NÚMERO DE CADA PRODUCTO EN EL CARRITO**********************************
-const cantidades = {};
+  // CREAR UN OBJETO VACÍO PARA CONTENER EL NÚMERO DE CADA PRODUCTO EN EL CARRITO*******************************
+  const cantidades = {};
 
-// RECORRER EL CARRITO Y ACTUALIZAR LAS CANTIDADES DE CADA PRODUCTO EN EL OBJETO*********************************
-carrito.forEach(producto => {
+  // RECORRER EL CARRITO Y ACTUALIZAR LAS CANTIDADES DE CADA PRODUCTO EN EL OBJETO******************************
+  carrito.forEach(producto => {
       if (producto.nombre in cantidades) {
         cantidades[producto.nombre]++;
       } else {
         cantidades[producto.nombre] = 1;
       }
-});
+  });
   
-// CREAR UN ELEMENTO LI PARA CADA PRODUCTO EN EL OBJETO Y AGREGARLO AL CARRITO***********************************
-for (const nombreProducto in cantidades) {
-  const cantidad = cantidades[nombreProducto];
-  const precio = productos.find(producto => producto.nombre === nombreProducto).precio;
-  const li = document.createElement("li");
-  li.innerText = `${cantidad} x ${nombreProducto} - $${(cantidad * precio).toFixed(2)}`;
-  productosCarrito.appendChild(li);
-}
+  // CREAR UN ELEMENTO LI PARA CADA PRODUCTO EN EL OBJETO Y AGREGARLO AL CARRITO*******************************
+  for (const nombreProducto in cantidades) {
+    const cantidad = cantidades[nombreProducto];
+    const precio = productos.find(producto => producto.nombre === nombreProducto).precio;
+    const li = document.createElement("li");
+    
+    // BOTON PARA REDUCIR LA CANTIDAD DE PRODUCTOS EN EL CARRITO **********************************************
+    const btnReduce = document.createElement("button");
+    btnReduce.textContent = "-";
+    btnReduce.addEventListener("click", () => {
+      const index = carrito.findIndex(p => p.nombre === nombreProducto);
+      carrito.splice(index, 1);
+      total -= precio;
+      mostrarCarrito();
+    });
+    
+    // BOTON PARA AUMENTAR LA CANTIDAD DE PRODUCTOS EN EL CARRITO **********************************************
+    const btnIncrease = document.createElement("button");
+    btnIncrease.textContent = "+";
+    btnIncrease.addEventListener("click", () => {
+      carrito.push(productos.find(p => p.nombre === nombreProducto));
+      total += precio;
+      mostrarCarrito();
+    });
+    
+    li.innerText = `${cantidad} x ${nombreProducto} - $${(cantidad * precio).toFixed(2)}`;
+    li.appendChild(btnReduce);
+    li.appendChild(btnIncrease);
+    productosCarrito.appendChild(li);
+  }
 
-  //  ACTUALIZAR EL TOTAL DE PRODUCTOS **************************************************************************
+  //  ACTUALIZAR EL TOTAL DE PRODUCTOS
   totalCarrito.innerText = `$${total.toFixed(0)}`;
 
-  // GUARDAR LA INFORMACIÓN DEL CARRITO ACTUALIZADA AL LOCALSTORAGE**********************************************
+  // GUARDAR LA INFORMACIÓN DEL CARRITO ACTUALIZADA AL LOCALSTORAGE
   localStorage.setItem('productosCarrito', JSON.stringify(carrito));
   localStorage.setItem('totalCarrito', total.toFixed(0));
 }
+
+
 
 // FUNCION PARA AGREGAR UN PRODUCTO AL CARRITO ******************************************************************
 function agregarProductoCarrito(id) {
